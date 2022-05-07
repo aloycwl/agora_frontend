6afef5c71c959ed381d6db0e4a44a07ca3dd3971
aloycwl <aloycwl@gmail.com>
2022-05-07 13:49:21 +0800

    1 file modified

--- js/load.js
+++ js/load.js
@@ -51,12 +51,13 @@ async function load() {
       if (ownerof.toLowerCase() == acct[0]) {
         tokenuri = formatURL(await contract.methods.tokenURI(id).call());
         if (tokenuri.length > 0) {
-          b = await $.getJSON(tokenuri);
-          img = formatURL(b.image);
+          //b = await $.getJSON(tokenuri);
+          $('#body').append(/*JSON.stringify(b)*/ tokenuri + '<br>');
+          /*img = formatURL(b.image);
           $('#body').append(
             `<div class="nfts"><b>${na} #${id}</b> - ${b.name}<br><i>${b.description}</i><br><img src="${img}">
             <button>Sell</button></div>`
-          );
+          );*/
         }
       }
     }
