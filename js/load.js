a52dac84ac680cc74bdb46a6d135879327b2bc1f
aloycwl <aloycwl@gmail.com>
2022-05-07 11:15:05 +0800

    2 files added

---
+++ js/load.js
@@ -0,0 +1,3 @@
+a = jQuery.getJSON(
+  'https://api-rinkeby.etherscan.io/api?module=account&action=tokennfttx&address=0x15eD406870dB283E810D5885e432d315C94DD0dd&startblock=0&endblock=999999999&sort=desc&apikey=561643C1V7AJIMWFSBUYGWE8WG3NDVHS67'
+);
