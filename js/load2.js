a1f329d66939eeb8e23858d7638734c7cd895f74
aloycwl <aloycwl@gmail.com>
2022-05-10 11:19:33 +0800

    1 file modified

--- js/load2.js
+++ js/load2.js
@@ -4,6 +4,88 @@ async function load(o) {
   count = 0;
   web3 = new Web3(ethereum);
   web3 = web3.eth;
+  if ((await web3.net.getId()) != 4) {
+    await ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x4' }],
+    });
+    location.reload();
+  }
+  contract = new web3.Contract(
+    [
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: '_id',
+            type: 'uint256',
+          },
+        ],
+        name: 'Buy',
+        outputs: [],
+        stateMutability: 'payable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: '_contractAddr',
+            type: 'address',
+          },
+          {
+            internalType: 'uint256',
+            name: '_tokenId',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: '_price',
+            type: 'uint256',
+          },
+        ],
+        name: 'Sell',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+      {
+        inputs: [
+          {
+            internalType: 'uint256',
+            name: 'batch',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'offset',
+            type: 'uint256',
+          },
+        ],
+        name: 'Show',
+        outputs: [
+          {
+            internalType: 'string[]',
+            name: 'tu',
+            type: 'string[]',
+          },
+          {
+            internalType: 'uint256[]',
+            name: 'price',
+            type: 'uint256[]',
+          },
+          {
+            internalType: 'uint256[]',
+            name: 'listId',
+            type: 'uint256[]',
+          },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+      },
+    ],
+    0x07b2de1b0d412d567ca4028ab229fd8a671f2491
+  );
   await fetch(
     `https://testnets-api.opensea.io/api/v1/assets?owner=${acct[0]}&order_direction=desc&offset=${o}&limit=50`,
     { method: 'GET' }
@@ -26,5 +108,28 @@ async function load(o) {
 }
 load(0);
 async function sell(addr, id) {
+  contract2 = new web3.Contract(
+    [
+      {
+        inputs: [
+          {
+            internalType: 'address',
+            name: 'to',
+            type: 'address',
+          },
+          {
+            internalType: 'uint256',
+            name: 'tokenId',
+            type: 'uint256',
+          },
+        ],
+        name: 'approve',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+    ],
+    0x07b2de1b0d412d567ca4028ab229fd8a671f2491
+  );
   alert(addr + ' ' + id);
 }
